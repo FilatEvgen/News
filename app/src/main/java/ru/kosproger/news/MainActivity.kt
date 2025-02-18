1e35ccf7dcd498a5d858efcbc34e4bcc72211e28
FilatEvgen <sergey.badyanov1@mail.ru>
2025-02-18 14:00:05 +0500

    First Commit

--- app/src/main/java/ru/kosproger/news/MainActivity.kt
+++ app/src/main/java/ru/kosproger/news/MainActivity.kt
@@ -1,25 +1,31 @@
 package ru.kosproger.news
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import ru.kosproger.news.databinding.ActivityMainBinding
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+
 
 class MainActivity : AppCompatActivity() {
 
-    private var _binding: ActivityMainBinding? = null
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(_binding!!.root)
-    }
-
-
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Handler(Looper.myLooper()!!).postDelayed({
+            setContentView(binding.root)
+            binding.bottonNavMenu.setupWithNavController(
+                navController = binding.bottonNavMenu.findNavController()
+            )
+        }, 5000)
     }
 }
 
