6fc8e46bd3e0a471c5c7b934a6005c94ba22be4c
FilatEvgen <sergey.badyanov1@mail.ru>
2025-02-19 12:31:30 +0500

    First Commit

--- app/src/main/java/ru/kosproger/news/MainActivity.kt
+++ app/src/main/java/ru/kosproger/news/MainActivity.kt
@@ -1,31 +1,34 @@
 package ru.kosproger.news
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import ru.kosproger.news.databinding.ActivityMainBinding
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-
+import ru.kosproger.news.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        Handler(Looper.myLooper()!!).postDelayed({
-            setContentView(binding.root)
-            binding.bottonNavMenu.setupWithNavController(
-                navController = binding.bottonNavMenu.findNavController()
-            )
-        }, 5000)
+
+        // Получите NavController из NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navController.navigate(R.id.splashFragment)
+        // Установите слушатель для навигации
+        binding.bottomNavMenu.setOnNavigationItemSelectedListener { item ->
+            when (val id = item.itemId) {
+                R.id.mainFragment, R.id.searchFragment, R.id.favoriteFragment -> {
+                    navController.navigate(id)
+                    true
+                }
+                else -> false
+            }
+        }
+
     }
 }
-
--- app/src/main/java/ru/kosproger/news/ui/splash/SplashFragment.kt
+++ app/src/main/java/ru/kosproger/news/ui/splash/SplashFragment.kt
@@ -1,12 +1,14 @@
 package ru.kosproger.news.ui.splash
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import ru.kosproger.news.R
-import ru.kosproger.news.databinding.FragmentDetailsBinding
 import ru.kosproger.news.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
@@ -17,7 +19,21 @@ class SplashFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentSplashBinding.inflate(layoutInflater,container,false)
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
         return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Задержка перед переходом к основному фрагменту
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
+        }, 2000)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
